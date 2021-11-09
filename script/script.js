f521262ea57335fd1e8a3b792feadc49a9cdb534
Davy Pereira <davypereira1802@gmail.com>
2021-11-09 20:10:51 -0300

    new design

--- script/script.js
+++ script/script.js
@@ -13,22 +13,8 @@ function resposta(){
     var div = document.getElementById('resposta')
   
 
-    div.innerHTML + `Simbolo = ${elementsinfo[(numeroatomico-1)]["simbolo"]}` + '<br>';
-    div.innerHTML += `Elemento: ${elementsinfo[(numeroatomico-1)]["nome"]}` + '<br>';
-    div.innerHTML += `Densidade : ${elementsinfo[(numeroatomico-1)]["densidade"]}` + '<br>';
-    div.innerHTML += `Massa Atomica  ${elementsinfo[(numeroatomico-1)]["matomica"]}` + '<br>';
-    div.innerHTML += `Distribuicao Eletronica  ${elementsinfo[(numeroatomico-1)]["eletronicconfig"]}` + '<br>';
-    div.innerHTML += `Ponto de fusao ${elementsinfo[(numeroatomico-1)]["fusao"]}` + '<br>';
-    div.innerHTML += `Ponto de ebulicao ${elementsinfo[(numeroatomico-1)]["ebulicao"]}` + '<br>';
-    div.innerHTML += `Coeficiente de Energia Molar :${elementsinfo[(numeroatomico-1)]["energiamolarcoef"]}` + '<br>';
-    div.innerHTML += `Coeficiente de Eletronegatividade  :${elementsinfo[(numeroatomico-1)]["eletronegatividade"]}` + '<br>';
-    div.innerHTML += `Aparencia  :${elementsinfo[(numeroatomico-1)]["aparencia"]}` + '<br>';
-    div.innerHTML += `Coloracao  :${elementsinfo[(numeroatomico-1)]["color"]}` + '<br>';
-    div.innerHTML += `Estado em temp Ambiente  :${elementsinfo[(numeroatomico-1)]["phase"]}` + '<br>';
-    div.innerHTML += `Descoberto Por  :${elementsinfo[(numeroatomico-1)]["descobertopor"]}` + '<br>';
-    div.innerHTML += `Familia  :${elementsinfo[(numeroatomico-1)]["familia"]}` + '<br>';
-    div.innerHTML += `Posicao Eixo X   :${elementsinfo[(numeroatomico-1)]["posX"]}` + '<br>';
-    div.innerHTML += `Posicao Eixo Y   :${elementsinfo[(numeroatomico-1)]["PosY"]}` + '<br>' + '<hr>';
+    div.innerHTML =
+      `Simbolo = ${elementsinfo[(numeroatomico - 1)]["simbolo"]}<br>Elemento: ${elementsinfo[(numeroatomico - 1)]["nome"]}<br>Densidade : ${elementsinfo[(numeroatomico - 1)]["densidade"]}<br>Massa Atomica  ${elementsinfo[(numeroatomico - 1)]["matomica"]}<br>Distribuicao Eletronica  ${elementsinfo[(numeroatomico - 1)]["eletronicconfig"]}<br>Ponto de fusao ${elementsinfo[(numeroatomico - 1)]["fusao"]}<br>Ponto de ebulicao ${elementsinfo[(numeroatomico - 1)]["ebulicao"]}<br>Coeficiente de Energia Molar :${elementsinfo[(numeroatomico - 1)]["energiamolarcoef"]}<br>Coeficiente de Eletronegatividade  :${elementsinfo[(numeroatomico - 1)]["eletronegatividade"]}<br>Aparencia  :${elementsinfo[(numeroatomico - 1)]["aparencia"]}<br>Coloracao  :${elementsinfo[(numeroatomico - 1)]["color"]}<br>Estado em temp Ambiente  :${elementsinfo[(numeroatomico - 1)]["phase"]}<br>Descoberto Por  :${elementsinfo[(numeroatomico - 1)]["descobertopor"]}<br>Familia  :${elementsinfo[(numeroatomico - 1)]["familia"]}<br>Posicao Eixo X   :${elementsinfo[(numeroatomico - 1)]["posX"]}<br>Posicao Eixo Y   :${elementsinfo[(numeroatomico - 1)]["PosY"]}<br><hr>`;
     
 
 }
